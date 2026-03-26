3621f385bcfe36fb6939a1ffe50ddab42df395db
Diego Pulido <diegopulido2d@gmail.com>
2026-03-25 22:11:15 -0500

    semantic header & footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-gray-800 w-full text-center py-6 text-sm">
+    <footer className="bg-gray-800 w-full text-center py-6 text-sm">
       {"© All rights reserved. Made by Diego Pulido, 2026"}
-    </div>
+    </footer>
   );
 };
 
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <Link
-      className="bg-gray-800 w-full text-center py-6 text-2xl font-bold text-white cursor-pointer"
-      href={"/"}
-      aria-label={`Icon back to products list`}
-    >
-      {"SEO Next Store"}
-    </Link>
+    <nav className="w-full bg-gray-800">
+      <Link
+        className="block w-full text-center py-6 text-2xl font-bold text-white hover:bg-gray-700 transition"
+        href={"/"}
+        aria-label="Go back to products list"
+      >
+        SEO Next Store
+      </Link>
+    </nav>
   );
 };
 
